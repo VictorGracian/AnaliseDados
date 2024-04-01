af1db419499c0afec6bb41e2fdbbb82cd8f31522
Victor Enrique Cavalheri Graciano <victor.graciano@duartecon.local>
2024-04-01 10:34:00 -0300

    funcoes e graficos

--- analisedados/Analise.py
+++ analisedados/Analise.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import plotly.express as px
 
 def ImportandoPlanilhas():
     df_principal = pd.read_excel("C:\\Users\\victor.graciano\\Desktop\\Imersão Python - Tabela de ações.xlsx", sheet_name="Principal")
@@ -34,3 +34,23 @@ def Merge():
 def Condicao():
     df_principal['Resultado'] = df_principal['variacao_rs'].apply(lambda x: 'Subiu' if x > 0 else ('Desceu' if x < 0 else 'Estavel'))
     df_principal
+    
+def funcoes():
+    maior = df_principal['variacao_rs'].max()
+    menor =  df_principal['variacao_rs'].min()
+    media =  df_principal['variacao_rs'].mean()
+    
+    media_subiu = df_principal[df_principal['Resultado'] == 'Subiu']['variacao_rs'].mean()
+    media_desceu = df_principal[df_principal['Resultado'] == 'Desceu']['variacao_rs'].mean()
+    
+    print(f'A variação maior é {maior:,.2f}')
+    print(f'A variação menor é {menor:,.2f}')
+    print(f'A media de variação é {media:,.2f}')
+    print(f'A media de quem subiu é {media_desceu:,.2f}')
+    
+def Analises():
+    df_analise_segmento =  df_principal.groupby('Resultado')['variacao_rs'].sum().reset_index()
+    df_analise_segmento
+
+def Grafico():
+    fig = px.bar(df_analise_segmento, x='Resultado', y='variacao_rs', text='variacao_rs', title='Variação reais por Resultado')
